07b2526722063c98283398a7a23bdf7856295752
kuenaimaku <kuenaimaku@gmail.com>
2021-06-21 05:26:48 -0500

    add _redirects for netlify

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,7 +3,7 @@
 		<div class="content has-text-centered">
 			<p>
 				<strong>ReIMAGINE Calculators</strong> by
-				<a href="https://github.com/kuenaimaku">Kuenaimaku</a>. The <a href="https://github.com/Kuenaimaku/apple-calculator">source code</a> is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+				<a href="https://github.com/kuenaimaku">Kuenaimaku</a>. The <a href="https://github.com/Kuenaimaku/reimagine-tools">source code</a> is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
 			</p>
 		</div>
 	</footer>
