96b7091e4d2fc772a0b8b60a853bba97b668f666
kuenaimaku <kuenaimaku@gmail.com>
2021-06-21 05:58:45 -0500

    router changes

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,31 @@ import Apples from "@/views/AppleView.vue";
 import Books from "@/views/BookView.vue";
 import Expertise from "@/views/ExpertiseView.vue";
 
+const DEFAULT_TITLE = 'ReIMAGINE Tools';
 const routes = [
 	{
 		path: "/",
 		name: "Home",
 		component: Home,
+		meta: { title: DEFAULT_TITLE }
 	},
 	{
 		path: "/apples",
 		name: "Apples",
 		component: Apples,
+		meta: { title: "Golden Apple Calculator" }
 	},
 	{
 		path: "/books",
 		name: "Books",
 		component: Books,
+		meta: { title: "Expertise Textbook Calculator" }
 	},
 	{
 		path: "/expertise",
 		name: "Expertise",
 		component: Expertise,
+		meta: { title: "Expertise Calculator" }
 	}
 ];
 
@@ -38,6 +43,14 @@ const router = createRouter({
 			}
 		}
 	},
+});
+
+router.afterEach((to, from) => {
+    // Use next tick to handle router history correctly
+    // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+    Vue.nextTick(() => {
+        document.title = to.meta.title(to) || DEFAULT_TITLE;
+    });
 });
 
 export default router;
